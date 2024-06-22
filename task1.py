64b424f0d7bb64d7ce3bfa3f0defeb6182e9cc88
HaseebAhmad23 <haseeb26@live.com>
2024-06-23 01:43:38 +0200

    adding switch and creating update method

--- task1.py
+++ task1.py
@@ -5,7 +5,6 @@ import json
 def get_data(url):
     response = requests.get(url)
     if response.status_code==200:
-        print(type(response.json()))
         return response.json()
     else:
         print('Could not fetch data from API')
@@ -20,22 +19,47 @@ def print_data(data):
 def post_data(url,payload):
     response = requests.post(url,data=payload)
     if response.status_code==201:
-        print(f'Data posted successfluy with status code:{response.status_code}')
+        print(f'Post is created successfuly with status code:{response.status_code}')
+    else:
+        print(f'Post creation is unsuccessful with status code:{response.status_code}')
+        print(f'Response Conetnt:{response.content}')
+
+def update_data(url,payload):
+    response = requests.put(url,data=payload)
+    if response.status_code==200:
+        print(f'Post is updated successfluy with status code:{response.status_code}')
+        print(f'Updated Post:{response.json()}')
+    else:
+        print(f'Post updation is unsuccessful with status code:{response.status_code}')
+        print(f'Response Conetnt:{response.content}')
 
 if __name__=='__main__':
-    url='https://jsonplaceholder.typicode.com/posts'
+    base_url='https://jsonplaceholder.typicode.com/posts'
 
-    answer= input('Do you want to read posts data?')
-    if answer== 'yes':
+    print('Select any one option')
+    print('1: Read Post' '\n' 
+          '2. Create Post' '\n'
+          '3. Update Post' '\n'
+          '2. Delete Post' '\n')
+    answer= input('Select number:')
+    if answer== '1':
         id = input('Enter id:')
-        get_url=f'{url}/{id}'
+        get_url=f'{base_url}/{id}'
         data = get_data(get_url)
         print(data)
 
-    answer= input('Do you want to create post data?')
-    if answer== 'yes':
+    elif answer== '2':
         userId = input('Enter userId:')
         id = input('Enter id:')
         body = input('Enter body:')
         payload={'userId':{userId},'id':{id},'body':{body}}
-        post_data(url,payload)
+        post_data(base_url,payload)
+
+    elif answer== '3':
+        id = input('Enter id:')
+        title = input('Enter title:')
+        body = input('Enter body:')
+        userId = input('Enter userId:')
+        payload={'id':{id},'title':{title} ,'body':{body}, 'userId':{userId}}
+        update_url=f'{base_url}/{id}'
+        update_data(update_url,payload)
